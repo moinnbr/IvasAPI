3aab370cca242f62054e1af19b417070feab7505
moin <moinur.hossain@yahoo.com>
2024-11-21 18:00:44 +0600

    Reformat Code

--- src/main/java/com/example/ivasapi/repository/IvasRecordRepository.java
+++ src/main/java/com/example/ivasapi/repository/IvasRecordRepository.java
@@ -2,6 +2,7 @@ package com.example.ivasapi.repository;
 
 import com.example.ivasapi.model.IvasRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface IvasRecordRepository extends JpaRepository<IvasRecord, Long> {
